7c38c88aa8cdafcd9ab49b87a1a27ea5a7c45752
Sergio R Pérez <37162018+SergioOwls@users.noreply.github.com>
2019-07-22 18:02:31 -0400

    The Village Game
    Final Commit.

---
+++ Village-Exploration Game/Assets/FollowTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public GameObject followTarget;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = this.transform.position - followTarget.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = followTarget.transform.position + offset;
+
+    }
+}
---
+++ Village-Exploration Game/Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float walkingSpeed;
+    public float rotationSpeed;
+
+    public LayerMask mask;
+
+    float horMov, verMov;
+    CharacterController charContr;
+	// Use this for initialization
+	void Start () {
+        charContr = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetAlignment();
+        Movement();
+	}
+
+    private void GetAlignment()
+    {
+        RaycastHit hit; Physics.Raycast(transform.position, -transform.up, out hit, 0.7f, mask);
+
+        Vector3 newUp = hit.normal;
+        transform.up = newUp; 
+    }
+
+    private void Movement()
+    {
+
+        horMov = Input.GetAxisRaw("Horizontal") * rotationSpeed;
+        verMov = Input.GetAxisRaw("Vertical") * walkingSpeed;
+
+        Vector3 newPosition = new Vector3(0, 0, verMov);
+        newPosition = transform.TransformDirection(newPosition);    // Makes rotation thing work.
+
+        charContr.Move(newPosition);
+        transform.Rotate(new Vector3(0, horMov, 0));
+
+        Debug.Log(horMov);
+
+    }
+}
